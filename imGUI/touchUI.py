e3d5755b0830b3567515276e93f05f3541503322
Reva Scharf <rscharf@bu.edu>
2016-01-27 15:56:49 -0500

    Edited test GUI for I2C to communicated with two MSP430s
    Implemented two buttons to send a signal to individual MSP430s over
    I2C.  When the MSP gets the signal, it toggles the on board LED.  Then
    the Pi asks to read from the MSP and reads the MSP’s address from the
    MSP.  The address is displayed on the button that was pushed initially.

--- imGUI/touchUI.py
+++ imGUI/touchUI.py
@@ -1,5 +1,7 @@
 import kivy
 import datetime
+import smbus
+import time
 
 from kivy.app import App
 from kivy.uix.widget import Widget
@@ -9,12 +11,32 @@ from kivy.uix.label import Label
 kivy.require('1.9.0')
 from kivy.uix.image import Image
 
+bus = smbus.SMBus(1)
+DEVICE1_ADDRESS = 0x50
+DEVICE2_ADDRESS = 0x12
+DEVICE_REG_MODE1 = 0x00
 
-class SendButton(Button):
+class Send1Button(Button):
     def on_press(self):
-        now = datetime.datetime.now()
-        self.text = 'minute is ' + str(now.minute)
+       # now = datetime.datetime.now()
+       # self.text = 'minute is ' + str(now.minute)
+        bus.write_byte_data(DEVICE1_ADDRESS, DEVICE_REG_MODE1, 4)
+        time.sleep(1)
+        bus.write_byte(DEVICE1_ADDRESS, 0xFF)
+        res = bus.read_byte(DEVICE1_ADDRESS)
+        self.text = 'Reading from device address ' + hex(res)
+       # time.sleep(4)
+       # self.text = 'Toggle LED on MSP430 1'
 
+class Send2Button(Button):
+    def on_press(self):
+	bus.write_byte_data(DEVICE2_ADDRESS, DEVICE_REG_MODE1, 4)
+        time.sleep(1)
+        bus.write_byte(DEVICE2_ADDRESS,0xFF)
+        res2 = bus.read_byte(DEVICE2_ADDRESS)
+        self.text = 'Reading from device address ' + hex(res2)
+       # time.sleep(4)
+       # self.text = 'Toggle LED on MSP430 2'
 
 class PtWidg(Widget):
     pass
