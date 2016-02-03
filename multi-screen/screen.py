d896adb063f08fd9276103826d747002d41b4789
Reva Scharf <rscharf@bu.edu>
2016-02-03 15:06:08 -0500

    added functionality to initial panel configuration menu

--- multi-screen/screen.py
+++ multi-screen/screen.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import StringProperty
 
 class HomeScreen(Screen):
     pass
@@ -16,14 +17,29 @@ class ManageUserProfilesScreen(Screen):
     pass
 
 class InitialPanelConfigScreen(Screen):
-    pass
+    panel_connect = StringProperty()
+    panelNo = 0
+
+    def __init__(self, **kwargs):
+        super(InitialPanelConfigScreen, self).__init__(**kwargs)
+        self.panel_connect = 'Connect Panel: ' + str(self.panelNo)
+
+    def panelConnected(self):
+        #python I2C code goes here to send actual address
+        if self.panelNo < 20:
+            self.panelNo += 1
+            self.panel_connect = 'Connect Panel: ' + str(self.panelNo)
+
+    def cancelButton(self):
+        #app.root.current = 'settings'
+        self.panelNo = 0
+        self.panel_connect = 'Connect Panel: ' + str(self.panelNo)
 
 class AdjustVolumeScreen(Screen):
     pass
 
 class CreateProfileScreen(Screen):
     pass
-    #textinput = TextInput(text='Hello world')
 
 class ScreenManagement(ScreenManager):
     pass
@@ -34,4 +50,5 @@ class MainApp(App):
     def build(self):
         return presentation
 
-MainApp().run()
+if __name__ == '__main__':
+    MainApp().run()
