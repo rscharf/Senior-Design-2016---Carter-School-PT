086279765bfc6d872007077047b4e078931a0662
Reva Scharf <rscharf@bu.edu>
2016-02-11 17:09:20 -0500

    Beginning of profile generation, clocking for user runs
    Added code to save user profiles to profiles.txt file.  Sample
    profiles.txt added.  Created a clock that runs startRun function on a
    set interval.  Set to 1 second for testing.  Will change granularity
    when actually working with I2C.  Currently just updates the screen
    label by incrementing the number by 1.

--- multi-screen/screen.py
+++ multi-screen/screen.py
@@ -1,12 +1,15 @@
+import time
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import StringProperty, ListProperty, ObjectProperty
+from kivy.properties import StringProperty, ListProperty, ObjectProperty, NumericProperty
 from kivy.uix.dropdown import DropDown
+from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.spinner import Spinner
 from kivy.uix.slider import Slider
+from kivy.clock import Clock
 
 
 class HomeScreen(Screen):
@@ -39,10 +42,6 @@ class InitialPanelConfigScreen(Screen):
         self.panelNo = 0
         self.panel_connect = 'Connect Panel: ' + str(self.panelNo)
 
-class AdjustVolumeScreen(Screen):
-    pass
-
-
 class CreateProfileScreen(Screen):
     spinner = ObjectProperty()
     nameinput = ObjectProperty()
@@ -50,15 +49,55 @@ class CreateProfileScreen(Screen):
     brightslide = ObjectProperty()
 
     def createprofile(self):
-        print('Chosen language for user', self.nameinput.text, 'is', self.spinner.text)
-        print('Volume set to', self.volslide.value)
-        print('Brightness set to', self.brightslide.value)
+        print('Chosen language for user ' + str(self.nameinput.text) + ' is ' + str(self.spinner.text))
+        print('Volume set to ' + str(self.volslide.value))
+        print('Brightness set to ' + str(self.brightslide.value))
+
+        f = open("profiles.txt", "a")
+        f.write(str(self.nameinput.text) + ", " + str(self.spinner.text) + ", " + str(self.volslide.value) + ", " + str(self.brightslide.value) + "\n")
+        f.close()
 
     def cancelProf(self):
         self.nameinput.text = ''
         self.spinner.text = 'Select Language'
         self.volslide.value = 0
         self.brightslide.value = 0
+
+class RunningScreen(Screen):
+    footMarkerStr = StringProperty()
+    footNum = NumericProperty()
+
+    def __init__(self, **kwargs):
+        super(RunningScreen, self).__init__(**kwargs)
+        self.footNum = 0
+        if (self.footNum == 1):
+            self.footMarkerStr = str(self.footNum) + ' foot'
+        else:
+            self.footMarkerStr = str(self.footNum) + ' feet'
+
+
+    def startScreen(self):
+        #schedule timer to check for i2c data
+        Clock.schedule_interval(self.startRun,1)
+
+    def cancelButt(self):
+        Clock.unschedule(self.startRun, all=True)
+        self.footNum = 0
+        self.footMarkerStr = str(self.footNum) + ' feet'
+
+    def startRun(self, dt):
+        #python i2c here for detecting sensor triggers and giving acks back for turning on lights, trigger sound
+
+        #update on screen labels
+        self.footNum+=1
+        if (self.footNum == 1):
+            self.footMarkerStr = str(self.footNum) + ' foot'
+        else:
+            self.footMarkerStr = str(self.footNum) + ' feet'
+
+        print('working - ' + str(self.footNum))
+
+
 
 class ScreenManagement(ScreenManager):
     pass
